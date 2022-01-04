cbbac28eb274a11d18d8c84b54e653c234e34948
Akash123-AKA <67219831+Akash123-AKA@users.noreply.github.com>
2022-01-04 19:19:31 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account tim = new Account("Ramesh", "R00001234");
+		
+		
+		Account beyonce = new Account("Suresh", "R00005678");
+		beyonce.showMenu();
+		
+		
+	}
+
+}
